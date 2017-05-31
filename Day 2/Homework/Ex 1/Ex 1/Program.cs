bc8abb2732f7735864a9e8d60b770c7412c89174
Georgi-Dimitrov <gdimitrov@shutterfly.com>
2017-05-31 12:11:51 +0300

    Revision
    Fixed issues with the task like errors in the algorithm and compilation
    errors.

--- Day 2/Homework/Ex 1/Ex 1/Program.cs
+++ Day 2/Homework/Ex 1/Ex 1/Program.cs
@@ -7,62 +7,11 @@ using System.Threading.Tasks;
 namespace Ex_1
 {
     class Program
-    {
-
+    {        
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Enter two numbers:");
-            int firstDecimal = int.Parse(Console.ReadLine());
-            int secondDecimal = int.Parse(Console.ReadLine());
-
-            string firstBinary = null;
-            //Converting decimal to binary
-            while (firstDecimal > 0)
-            {
-                firstDecimal = firstDecimal / 2;
-                int remainder = firstDecimal % 2;
-                firstBinary = firstBinary + remainder.ToString();
-            }
-            Console.WriteLine(/*int.Parse(*/"binary: " + firstBinary);
-
-            string secondBinary = null;
-            while (secondDecimal > 0)
-            {
-                secondDecimal = secondDecimal / 2;
-                int remainderTwo = secondDecimal % 2;
-                secondBinary = secondBinary + remainderTwo.ToString();
-            }
-            Console.WriteLine("binary: " + secondBinary);
-
-
-
-            int.Parse(firstBinary);
-            int.Parse(secondBinary);
-
-            int[] firstArray;
-            for (int i = 0; i < 20; i++)
-            {
-                firstArray[i] = firstBinary % 10;
-            }
-
-            int sum = 0;
-            for (int i = 0; i < firstArray.Length; i++)
-            {
-                sum = sum + firstArray[i];
-            }
-            //...
-
-
-             
-
-
-          }
-
-
-
-
-
+           Console.WriteLine(MathProblems.SumBinaryRepresentations(50, 80));
         }
     }
+}
 
